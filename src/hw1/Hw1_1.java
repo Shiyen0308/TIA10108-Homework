5ce7a232a9eef4f52deb2e71ebebe28418772f8a
Shiyen Huang <qaswedfr0308@gmail.com>
2024-02-03 16:44:38 +0800

    modified hw1_1

--- src/hw1/Hw1_1.java
+++ src/hw1/Hw1_1.java
@@ -4,7 +4,6 @@ package hw1;
 public class Hw1_1 {
 	public static void main(String[] args) {
 		int num1 = 12, num2 = 6;
-		System.out.println("和為 " + (num1 + num2) + "\n" + 
-							"積為 " + (num1 * num2));
+		System.out.println("和為 " + (num1 + num2) + "\n" + "積為 " + (num1 * num2));
 	}
 }
